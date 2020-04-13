9556cea21731641de6a25d8c3a30d3e2ac1ce445
XuGuiYuan <xgy@jxsec.cn>
2020-04-13 19:10:09 +0800

    add passport

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -24,6 +24,13 @@
       0 => 'Fruitcake\\Cors\\CorsServiceProvider',
     ),
   ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
   'laravel/tinker' => 
   array (
     'providers' => 
